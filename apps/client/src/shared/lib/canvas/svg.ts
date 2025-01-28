e8055879e06ca793e2c45501047da58837caa87f
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2025-01-28 20:19:29 +0900

    feat: Added svg utils that calculate screen or svg point

---
+++ apps/client/src/shared/lib/canvas/svg.ts
@@ -0,0 +1,33 @@
+import type { Point } from '@/shared/types/canvas';
+
+const createSvgPoint = (svg: SVGSVGElement, point: Point): SVGPoint => {
+    const svgPoint = svg.createSVGPoint();
+    svgPoint.x = point.x;
+    svgPoint.y = point.y;
+    return svgPoint;
+};
+
+const getScreenCTM = (svg: SVGSVGElement): DOMMatrix => {
+    const screenCTM = svg.getScreenCTM();
+    if (!screenCTM) {
+        throw new Error('getScreenCTM: screenCTM이 존재하지 않습니다.');
+    }
+    return screenCTM;
+};
+
+const transformPoint = (point: SVGPoint, matrix: DOMMatrix): Point => ({
+    x: point.matrixTransform(matrix).x,
+    y: point.matrixTransform(matrix).y,
+});
+
+export const screenToSvgPoint = (svg: SVGSVGElement, point: Point): Point => {
+    const svgPoint = createSvgPoint(svg, point);
+    const screenCTM = getScreenCTM(svg);
+    return transformPoint(svgPoint, screenCTM.inverse());
+};
+
+export const svgToScreenPoint = (svg: SVGSVGElement, point: Point): Point => {
+    const svgPoint = createSvgPoint(svg, point);
+    const screenCTM = getScreenCTM(svg);
+    return transformPoint(svgPoint, screenCTM);
+};
