d40f0672d9c37dc964565be8c48fc37d1cc9418a
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2025-02-07 15:46:50 +0900

    refactor: remove unused getCenterGridPoint function

--- apps/client/src/widgets/resource-controls/hooks/use-controls-point.ts
+++ apps/client/src/widgets/resource-controls/hooks/use-controls-point.ts
@@ -7,7 +7,6 @@ import { useNodeStore } from '@/entities/node/model/node.store';
 
 import { GRID_SIZE_2D, GRID_WIDTH_3D } from '@/shared/config/canvas';
 import { gridToCoordPoint } from '@/shared/lib/canvas/point';
-import { getCenterGridPoint } from '@/shared/lib/canvas/svg';
 
 export const useControlsPoint = (nodeId: string | null, gap: number) => {
     const nodes = useNodeStore.use.nodes();
@@ -18,12 +17,11 @@ export const useControlsPoint = (nodeId: string | null, gap: number) => {
 
         const node = nodes[nodeId];
         const cols = node.size[viewMode].cols;
-        const centerPoint = getCenterGridPoint(node.point, node.size[viewMode]);
 
         const width =
             viewMode === '2d' ? GRID_SIZE_2D * cols : GRID_WIDTH_3D * cols;
 
-        const pixels = gridToCoordPoint(centerPoint, viewMode);
+        const pixels = gridToCoordPoint(node.point, viewMode);
 
         //INFO: block 형태는 그리는 기준점이 달라 필터처리
         const margin = viewMode === '3d' ? width / 2 : width;
