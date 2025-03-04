c3fd1d19dd03104ee90965571b4a547471f534f9
p1n9d3v <p1n9m4i1@gmail.com>
2025-03-04 17:07:56 +0900

    feat: render group using polygon based on grid points
    The group component now renders a polygon based on the provided grid points. It converts grid coordinates to canvas coordinates and uses these to define the polygon's shape. This allows for more flexible and dynamic group representations on the canvas.

--- apps/client/src/entities/group/ui/group.tsx
+++ apps/client/src/entities/group/ui/group.tsx
@@ -1,25 +1,26 @@
 import { IsoMatrix } from '@/shared/config/canvas';
+import { gridToCoordPoint } from '@/shared/lib/canvas/point';
 import { GridPoint, ViewMode } from '@/shared/types/canvas';
 import { Polygon } from '@/shared/ui/svg/common/polygon';
 
 interface GroupProps {
-    points: GridPoint[];
+    points: Array<GridPoint>;
     viewMode: ViewMode;
+    padding?: number;
 }
 
 export const Group = (props: GroupProps) => {
     const { viewMode, points } = props;
     const transform = viewMode === '3d' ? IsoMatrix?.toString() : undefined;
 
+    const coordPoints = points.map((point) => {
+        return gridToCoordPoint(point, '2d');
+    });
+
     return (
         <Polygon
             transform={transform}
-            points={[
-                { x: 0, y: 0 },
-                { x: 90, y: 0 },
-                { x: 90, y: 90 },
-                { x: 0, y: 90 },
-            ]}
+            points={coordPoints}
             fill="none"
             stroke="black"
             strokeWidth={3}
